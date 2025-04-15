97ea2448d0ba9ccbaed441b3a5b72dd849278c6d
Mrunal Gaikwad <gaikwadmrunal7212@gmail.com>
2025-04-15 22:22:03 +0530

    Updated .env and database configuration

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
 import pg from "pg";
 import dotenv from "dotenv";
 dotenv.config();
-
+const{DB_URL}=process.env;
 const database = new pg.Client({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT||5433
+    connectionString:DB_URL,
+    ssl:{
+        rejectUnauthorized:false
+    }
 });
 
 database.connect((err)=>{
